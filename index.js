e57395cf9c27c6375ac71b77363b5c1a130a05f8
Majogobla <marcosjgodoyb@gmail.com>
2023-05-19 16:56:40 -0600

    arreglo del puerto en el archivo index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.use((error, req, res) =>
 
 // Dejar que Heroku asigne el puerto
 const host = '0.0.0.0';
-const port = process.env.PUERTO;
+const port = process.env.PORT || 5000;
 
 app.listen(port, host, () =>
 {
